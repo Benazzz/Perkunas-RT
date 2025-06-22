e463a7a037e97fbe07e3e843273eeb5ed29635c7
Benazzz <178609468+Benazzz@users.noreply.github.com>
2025-06-22 16:06:23 +0300

    test

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'benas/perkunas-rt',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b3e0ead0f0bb304da332a1fc378b8cc1459aa18d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'benas/perkunas-rt' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b3e0ead0f0bb304da332a1fc378b8cc1459aa18d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.10.0',
+            'version' => '6.10.0.0',
+            'reference' => 'bf74d75a1fde6beaa34a0ddae2ec5fce0f72a144',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
